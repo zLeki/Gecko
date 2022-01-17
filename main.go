5fc11a6d3255ce4890902165bfe589bfaa9bf268
zLeki <85647342+zLeki@users.noreply.github.com>
2022-01-17 13:22:38 -0500

    Main

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"github.com/fatih/color"
+	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"time"
 )
+
 var words = []string{"virus!!!", "asm monkey", "leki shit coder fr fr", "github.com/zLeki", "idk", "buy my merch"}
+
 func title() {
 	rand.Seed(time.Now().UnixNano())
 	random := rand.Intn(len(words))
@@ -24,23 +30,87 @@ func title() {
 		|___/					      
 
 		Gecko • Created by Leki#6796
-                        `+words[random]+`
+                        ` + words[random] + `
 	`)
 
 }
 func clear() {
-	fmt.Print("\033[H\033[2J")
+	for i := 0; i < 100; i++ {
+		fmt.Print("\n")
+	}
+}
+func dmSpam() {
+	type settings struct {
+		Token   string `json:"token"`
+		Guild   string `json:"guild"`
+		Threads int    `json:"threads"`
+	}
+	var data settings
+	dataFile, _ := ioutil.ReadFile("settings.json")
+	err := json.Unmarshal(dataFile, &data)
+	if err != nil {
+		log.Fatalf("error unmarshaling settings.json: %v", err)
+	}
+
+}
+func settings() {
+
+}
+func menu() {
+	title()
+	color.Yellow("[1] Dm spam            [2] Settings")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	if input.Text() == "1" {
+		title()
+		for i := 0; i < 10; i++ {
+			go dmSpam()
+		}
+		for {
+			time.Sleep(900 * time.Second)
+		}
+	} else if input.Text() == "2" {
+		settings()
+	}
 }
 func login() {
-	color.Yellow("[i] Email address: >>>") // would use a sql database but i dont want a monkey with my oracle online login
-	scan, err := fmt.Scan()
-	if err != nil {
-		return
+	type jsonData struct {
+		Users []struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		} `json:"users"`
 	}
-	log.Println(scan)
-	color.Yellow("[i] Password: >>>")
-	
+
+	color.Yellow("[i] Email address: >>> ") // would use a sql database but i dont want a monkey with my oracle online login
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	email := input.Text()
+	color.Yellow("[i] Password: >>> ")
+	input2 := bufio.NewScanner(os.Stdin)
+	input2.Scan()
+	password := input2.Text()
+	jsonFile, _ := ioutil.ReadFile("users.json")
+	var data jsonData
+	err := json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		log.Fatalf("Error occured while decoding json: %v", err)
+	}
+	for _, v := range data.Users {
+		if v.Email == email {
+			if v.Password == password {
+				title()
+				color.Green("[V] Welcome back, %v", v.Email)
+				time.Sleep(2 * time.Second)
+				menu()
+				return
+			}
+		}
+	}
+	log.Println("Unknown email or password")
+	time.Sleep(3 * time.Second)
+	os.Exit(0)
 }
 func main() {
 	title()
+	login()
 }

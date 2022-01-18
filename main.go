4d389ec4ac4e722ac01cdc7a57710a5c3f145059
zLeki <bennyfoefenny@icloud.com>
2022-01-18 15:50:47 -0500

    Revamped everything Now using REST api

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/fatih/color"
-	"github.com/zLeki/Gecko/structs"
 	"io"
 	"io/ioutil"
 	"log"
@@ -20,6 +19,87 @@ import (
 
 var client http.Client
 var words = []string{"virus!!!", "asm monkey", "leki shit coder fr fr", "github.com/zLeki", "idk", "buy my merch"}
+
+type ChannelData []struct {
+	ID              string        `json:"id"`
+	Type            int           `json:"type"`
+	Content         string        `json:"content"`
+	ChannelID       string        `json:"channel_id"`
+	Attachments     []interface{} `json:"attachments"`
+	Embeds          []interface{} `json:"embeds"`
+	Mentions        []interface{} `json:"mentions"`
+	MentionRoles    []interface{} `json:"mention_roles"`
+	Pinned          bool          `json:"pinned"`
+	MentionEveryone bool          `json:"mention_everyone"`
+	Tts             bool          `json:"tts"`
+	Timestamp       time.Time     `json:"timestamp"`
+	EditedTimestamp interface{}   `json:"edited_timestamp"`
+	Flags           int           `json:"flags"`
+	Components      []interface{} `json:"components"`
+	Author          struct {
+		ID            string `json:"id"`
+		Username      string `json:"username"`
+		Avatar        string `json:"avatar"`
+		Discriminator string `json:"discriminator"`
+		PublicFlags   int    `json:"public_flags"`
+		Bot           bool   `json:"bot"`
+	} `json:"author,omitempty"`
+
+	MessageReference struct {
+		ChannelID string `json:"channel_id"`
+		GuildID   string `json:"guild_id"`
+		MessageID string `json:"message_id"`
+	} `json:"message_reference,omitempty"`
+	ReferencedMessage struct {
+		ID        string `json:"id"`
+		Type      int    `json:"type"`
+		Content   string `json:"content"`
+		ChannelID string `json:"channel_id"`
+		Author    struct {
+			ID            string `json:"id"`
+			Username      string `json:"username"`
+			Avatar        string `json:"avatar"`
+			Discriminator string `json:"discriminator"`
+			PublicFlags   int    `json:"public_flags"`
+		} `json:"author"`
+		Attachments     []interface{} `json:"attachments"`
+		Embeds          []interface{} `json:"embeds"`
+		Mentions        []interface{} `json:"mentions"`
+		MentionRoles    []interface{} `json:"mention_roles"`
+		Pinned          bool          `json:"pinned"`
+		MentionEveryone bool          `json:"mention_everyone"`
+		Tts             bool          `json:"tts"`
+		Timestamp       time.Time     `json:"timestamp"`
+		EditedTimestamp time.Time     `json:"edited_timestamp"`
+		Flags           int           `json:"flags"`
+		Components      []interface{} `json:"components"`
+	} `json:"referenced_message,omitempty"`
+}
+type GuidldChannelData []struct {
+	ID                   string      `json:"id"`
+	LastMessageID        string      `json:"last_message_id,omitempty"`
+	LastPinTimestamp     time.Time   `json:"last_pin_timestamp,omitempty"`
+	Type                 int         `json:"type"`
+	Name                 string      `json:"name"`
+	Position             int         `json:"position"`
+	ParentID             string      `json:"parent_id"`
+	Topic                interface{} `json:"topic,omitempty"`
+	GuildID              string      `json:"guild_id"`
+	PermissionOverwrites []struct {
+		ID       string `json:"id"`
+		Type     string `json:"type"`
+		Allow    int    `json:"allow"`
+		Deny     int    `json:"deny"`
+		AllowNew string `json:"allow_new"`
+		DenyNew  string `json:"deny_new"`
+	} `json:"permission_overwrites"`
+	Nsfw             bool        `json:"nsfw"`
+	RateLimitPerUser int         `json:"rate_limit_per_user,omitempty"`
+	Banner           interface{} `json:"banner,omitempty"`
+	Bitrate          int         `json:"bitrate,omitempty"`
+	UserLimit        int         `json:"user_limit,omitempty"`
+	RtcRegion        interface{} `json:"rtc_region,omitempty"`
+}
 
 func title() {
 	rand.Seed(time.Now().UnixNano())
@@ -123,7 +203,7 @@ func Pullids(guildid string) {
 	req1.Header.Set("content-type", "application/json")
 	req1.Header.Set("Authorization", token)
 	resp1, _ := client.Do(req1)
-	var data1 structs.GuidldChannelData
+	var data1 GuidldChannelData
 	err = json.NewDecoder(resp1.Body).Decode(&data1)
 	if err != nil {
 		return
@@ -142,7 +222,7 @@ func Pullids(guildid string) {
 
 				}
 			}(resp.Body)
-			var data structs.ChannelData
+			var data ChannelData
 			err := json.NewDecoder(resp.Body).Decode(&data)
 			if err != nil {
 				log.Fatalf("Error decoding JSON: %v", err)
